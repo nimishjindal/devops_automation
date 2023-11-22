5b9e6ed753db17cb1a53bbf452892c76dc09c4e8
nimishjindal <nimish.jindal@yahoo.com>
2023-11-21 19:08:20 -0500

    Get IP from docker network command

--- setup_Jenkins.sh
+++ setup_Jenkins.sh
@@ -1,5 +1,15 @@
 # ----- host IP address ---------
-PRIVATE_IP=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | grep -Fv 10.0.0.63 | awk '{print $2}')
+
+NETWORK="jenkins-automated"
+
+NETWORK1=$(jq '.[0] | .Containers | map_values(select(.Name | contains("Sonar"))) ' <<< "$(docker network inspect $NETWORK)")
+
+IP_W_SLSH=$(jq 'map(.) | .[0] |.IPv4Address' <<< "$NETWORK1")
+
+IP_W_QUOTE=$(tr "/" " " <<< $IP_W_SLSH)
+
+PRIVATE_IP="$(echo $IP_W_QUOTE | tr "\"" " " | awk '{print $1}')"
+
 echo "$PRIVATE_IP" > pvt_ip_secret.txt
 
 echo "------- setup Jenkins ------------"
